770ba20f0e1b3dc772ba0ea4c08d9ca949d7c363
bia.duque <biaduque7@hotmail.com>
2021-11-24 17:52:50 -0300

    Adicionar grupos de codigos

---
+++ TheMidway/TheMidway/controller/EncontrosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EncontrosViewController.swift
+//  TheMidway
+//
+//  Created by Beatriz Duque on 24/11/21.
+//
+
+import UIKit
+
+class EncontrosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
