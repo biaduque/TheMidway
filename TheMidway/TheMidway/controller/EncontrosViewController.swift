7dc980b8b21983dc6344e31842566c24c6e43160
Felipe Torales Leite <felipetolei@hotmail.com>
2021-12-01 16:22:27 -0300

    Adicionado funcionalidade TableView

--- TheMidway/TheMidway/controller/EncontrosViewController.swift
+++ TheMidway/TheMidway/controller/EncontrosViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class EncontrosViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 }
+extension EncontrosViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        ///clique na celula
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadInputViews()
+    }
+}
+
+
+
+extension EncontrosViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "encontroCell", for: IndexPath(index: indexPath.row)) as! EncontrosTableViewCell
+        return cell
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+}
+
---
+++ TheMidway/TheMidway/view/EncontrosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EncontrosTableViewCell.swift
+//  TheMidway
+//
+//  Created by Felipe Leite on 01/12/21.
+//
+
+import UIKit
+
+class EncontrosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
