c4cbc52d7ecf7924d3bf8fdba76a7448f353df87
bia.duque <biaduque7@hotmail.com>
2021-12-02 14:07:23 -0300

    Adicionar primeira base para a tableView

---
+++ TheMidway/TheMidway/view/AmigosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AmigosTableViewCell.swift
+//  TheMidway
+//
+//  Created by Beatriz Duque on 02/12/21.
+//
+
+import UIKit
+
+class AmigosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
