319503b8a376331f2b2590630eeb769b54672c4e
bia.duque <biaduque7@hotmail.com>
2021-12-15 17:42:44 -0300

    Adicionar enderecos no AppClips

--- TheMidway/TheMidway/View/CollectionCell/SugestoesCollectionViewCell.swift
+++ TheMidway/TheMidway/View/CollectionCell/SugestoesCollectionViewCell.swift
@@ -17,15 +17,37 @@ class SugestoesCollectionViewCell: UICollectionViewCell {
     
     let images = ["Bakery", "Bar", "Cinema", "Coffee", "Restaurant", "Shopping", "Theater"]
 
-    let names = ["Bakery", "Bar", "Cinema", "Coffee", "Restaurant", "Shopping", "Theater"]
+    let names = ["Padaria", "Bar", "Cinema", "Café", "Restaurante", "Shopping", "Teatro"]
+    
+    let nomeLocalArray = ["Mercatto",
+                          "Igrejinha Bar",
+                          "Cinépollis",
+                          "Starbucks",
+                          "Bananeira",
+                          "Eldorado",
+                          "Theatro Municipal do RJ"
+                        ]
+    let enderecos = ["Av. Nascimento de Castro, 1890 - Lagoa Nova, Natal - RN, 59056-450",
+        "Rua Fernando de Albuquerque - SP, BR - Consolação",
+        "Alameda Rio Negro - Iguatemi, SP - Alphaville",
+        "Kalanderpl. 1, 8045 Zürich - ZH, Wiedikon",
+        "R. Mal. Hastimphilo de Moura,419 - SP, Morumbi",
+        "Av. Rebouças,  3970 - SP, Pinheiros",
+        "Praça Floriano, S/N - Centro, Rio de Janeiro - RJ, 20031-050, Brasil"
+    ]
     
     @IBOutlet weak var imageBackground: UIImageView!
     @IBOutlet weak var labelNome: UILabel!
+    @IBOutlet weak var enderecoLabel: UILabel!
+    @IBOutlet weak var nomeLocal: UILabel!
+    
     
     func stylize(index: Int){
         imageBackground.image = UIImage(named: images[index])
         imageBackground.layer.cornerRadius = 8
         labelNome.text = names[index]
+        nomeLocal.text = nomeLocalArray[index]
+        enderecoLabel.text = enderecos[index]
     }
     func didChange() {
         if setButton.imageView?.image == UIImage(systemName: "circle"){
