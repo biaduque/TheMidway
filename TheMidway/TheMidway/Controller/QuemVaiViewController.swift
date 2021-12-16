aaef47d0b6654fb35af5aaf3fbfe97e97aaff3f8
bia.duque <biaduque7@hotmail.com>
2021-12-16 00:13:39 -0300

    Corrigir Bug dos amiguinhos e da permissão

--- TheMidway/TheMidway/Controller/QuemVaiViewController.swift
+++ TheMidway/TheMidway/Controller/QuemVaiViewController.swift
@@ -39,13 +39,15 @@ class QuemVaiViewController: UIViewController, CNContactPickerDelegate, CNContac
     override func viewWillAppear(_ animated: Bool) {
         ///se a pessoa aceitar compartilhar os contatos, esse vetor estara preenchido
         contactsRequest = Contacts.shared.fatchContacts()
+        if contactsRequest.count == 0 {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
     }
     
     
@@ -63,10 +65,14 @@ class QuemVaiViewController: UIViewController, CNContactPickerDelegate, CNContac
     }
     
     @IBAction func addButton(_ sender: Any) {
-        let contactViewController = CNContactPickerViewController()
-        contactViewController.delegate = self
-        self.present(contactViewController, animated: true)
-        
+        if contactsRequest.count == 0 {
+            self.navigationController?.popViewController(animated: true)
+        }
+        else{
+            let contactViewController = CNContactPickerViewController()
+            contactViewController.delegate = self
+            self.present(contactViewController, animated: true)
+        }
     }
     
     // MARK: Contact Picker
