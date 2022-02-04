46015e8194dad5b05484270ec65a35772a2099e3
Gui Reis <gui.sreis25@gmail.com>
2022-02-04 17:42:35 -0300

    Removendo print

--- TheMidway/TheMidway/Controller/SuggestionsViewController.swift
+++ TheMidway/TheMidway/Controller/SuggestionsViewController.swift
@@ -199,7 +199,6 @@ class SuggestionsViewController: UIViewController, SuggestionsControllerDelegate
         self.apiManeger.getSuggetions(with: self.mainWord) { result in
             switch result {
             case .success(let suggestionsPlaces):
-                print("Deu certo \(suggestionsPlaces)")
                 self.suggestionsPlaces = suggestionsPlaces
                 
                 DispatchQueue.main.async {
@@ -207,146 +206,8 @@ class SuggestionsViewController: UIViewController, SuggestionsControllerDelegate
                 }
 
             case .failure(let error):
-                print("Deu erro \(error.description)")
                 self.mainView.setEmptyViewText(error.localizedDescription)
             }
         }
-        
-        
     }
-    
-    
-//    private func getSuggestions() -> Void {
-//        self.suggestionsPlaces = [
-//            MapPlace(
-//                name: "Muza",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.495333, longitude: -46.868243),
-//                pin: nil,
-//                type: .restaurant,
-//                addressInfo: AddressInfo(
-//                    postalCode: "06414-007",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "Barueri",
-//                    address: "Avenida Sebastião Davino dos Reis",
-//                    number: "101"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Gui Reis",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.713213, longitude: -46.536622),
-//                pin: nil,
-//                type: .nightlife,
-//                addressInfo: AddressInfo(
-//                    postalCode: "09770-200",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Bernardo do Campo",
-//                    address: "Rua Nicola Spinelli",
-//                    number: "469"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Leh",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.627604, longitude: -46.637000),
-//                pin: nil,
-//                type: .cafe,
-//                addressInfo: AddressInfo(
-//                    postalCode: "04304-000",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Paulo",
-//                    address: "Avenida Fagundes Filho",
-//                    number: "470"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Muza",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.495333, longitude: -46.868243),
-//                pin: nil,
-//                type: .restaurant,
-//                addressInfo: AddressInfo(
-//                    postalCode: "06414-007",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "Barueri",
-//                    address: "Avenida Sebastião Davino dos Reis",
-//                    number: "101"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Gui Reis",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.713213, longitude: -46.536622),
-//                pin: nil,
-//                type: .nightlife,
-//                addressInfo: AddressInfo(
-//                    postalCode: "09770-200",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Bernardo do Campo",
-//                    address: "Rua Nicola Spinelli",
-//                    number: "469"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Leh",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.627604, longitude: -46.637000),
-//                pin: nil,
-//                type: .cafe,
-//                addressInfo: AddressInfo(
-//                    postalCode: "04304-000",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Paulo",
-//                    address: "Avenida Fagundes Filho",
-//                    number: "470"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Muza",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.495333, longitude: -46.868243),
-//                pin: nil,
-//                type: .restaurant,
-//                addressInfo: AddressInfo(
-//                    postalCode: "06414-007",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "Barueri",
-//                    address: "Avenida Sebastião Davino dos Reis",
-//                    number: "101"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Gui Reis",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.713213, longitude: -46.536622),
-//                pin: nil,
-//                type: .nightlife,
-//                addressInfo: AddressInfo(
-//                    postalCode: "09770-200",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Bernardo do Campo",
-//                    address: "Rua Nicola Spinelli",
-//                    number: "469"
-//                )
-//            ),
-//            MapPlace(
-//                name: "Leh",
-//                coordinates: CLLocationCoordinate2D(latitude: -23.627604, longitude: -46.637000),
-//                pin: nil,
-//                type: .cafe,
-//                addressInfo: AddressInfo(
-//                    postalCode: "04304-000",
-//                    country: "BR",
-//                    city: "SP",
-//                    district: "São Paulo",
-//                    address: "Avenida Fagundes Filho",
-//                    number: "470"
-//                )
-//            )
-//        ]
-//
-//        self.suggestionsCollectionDataSource.setPlacesFound(self.suggestionsPlaces)
-//        self.reloadCollectionData()
-//    }
 }
